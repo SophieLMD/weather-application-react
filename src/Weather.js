58d45edc1fbe569d4fdfb247f4fa9158566213c1
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-01 19:20:56 +0100

    Updated Weather.js with basic html

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,12 @@
 import React from "react";
-import ".Weather.css";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div>Hello from weather</div>;
+  <div className="WeatherInfo">
+    <h1>New York</h1>
+    <ul>
+      <li>Humidity:</li>
+      <li>Wind:</li>
+    </ul>
+  </div>;
 }
