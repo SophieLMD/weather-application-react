e904e236074f39bfd09bc8aeff61b638a2468be2
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-01 19:09:10 +0100

    imported bootstrap and installed on App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Weather APP</p>
-      </header>
-      <footer>
-        Coded by {}{" "}
-        <a href="https://github.com/SophieLMD" target="_blank" rel="noreferrer">
-          Sophie Davies
-        </a>
-        , open sourced on {}{" "}
-        <a
-          href="https://github.com/SophieLMD/weather-application-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github {}
-        </a>{" "}
-        and website hosted on {}{" "}
-        <a
-          href="https://weather-application-react-slmd.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          Coded by {}{" "}
+          <a
+            href="https://github.com/SophieLMD"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sophie Davies
+          </a>
+          , open sourced on {}{" "}
+          <a
+            href="https://github.com/SophieLMD/weather-application-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github {}
+          </a>{" "}
+          and website hosted on {}{" "}
+          <a
+            href="https://weather-application-react-slmd.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
