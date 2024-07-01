bc52c1fb5a3d0bfc77babf9f22f6f74a026e24a0
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-01 19:12:34 +0100

    Created new component Weather.js and imported inside App.js, created Weather.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <footer>
           Coded by {}{" "}
           <a
