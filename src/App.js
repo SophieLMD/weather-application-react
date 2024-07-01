8faa385e3d6656bad83214d79476a4c879ebf936
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-01 18:47:05 +0100

    Added footer with links to GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ function App() {
       <header className="App-header">
         <p>Weather APP</p>
       </header>
+      <footer>
+        Coded by {}{" "}
+        <a href="https://github.com/SophieLMD" target="_blank" rel="noreferrer">
+          Sophie Davies
+        </a>
+        , open sourced on {}{" "}
+        <a
+          href="https://github.com/SophieLMD/weather-application-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github {}
+        </a>{" "}
+        and website hosted on {}{" "}
+        <a
+          href="https://weather-application-react-slmd.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
