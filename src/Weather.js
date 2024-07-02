8dcc03eee3a3ac9e4d78098d76024905c1beab01
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-02 06:00:59 +0100

    Completed basic html structure in Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,11 +2,32 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  <div className="WeatherInfo">
-    <h1>New York</h1>
-    <ul>
-      <li>Humidity:</li>
-      <li>Wind:</li>
-    </ul>
-  </div>;
+  return (
+    <div className="WeatherInfo">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          className="form-entry"
+        />
+        <input type="submit" value="Search" className="btn-primary" />
+      </form>
+      <h1>New York</h1>
+      <img
+        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+        alt="Mostly Cloudy"
+      />
+      6°
+      <ul>
+        <li>Precipitation:2%</li>
+        <li>Humidity:83%</li>
+        <li>Wind:7mph</li>
+      </ul>
+      <h2>Weather</h2>
+      <ul>
+        <li>Tuesday 05:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+    </div>
+  );
 }
