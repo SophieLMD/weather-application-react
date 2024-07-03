7869a7cd91caa1f27b537d8970d56d75e9b3f73b
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-03 15:30:19 +0100

    Styled page using CSS and Bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -4,33 +4,63 @@ export default function Weather() {
   return (
     <div className="weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="form-entry"
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-primary w-100 shadow-sm"
-        />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-entry"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100 shadow-sm"
+            />
+          </div>
+        </div>
       </form>
       <h1>New York</h1>
-      <img
-        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-        alt="Mostly Cloudy"
-      />
-      6°
-      <ul>
-        <li>Precipitation:2%</li>
-        <li>Humidity:83%</li>
-        <li>Wind:7mph</li>
-      </ul>
-      <h2>Weather</h2>
-      <ul>
-        <li>Tuesday 05:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
+      <div className="row mt-3 mb-4">
+        <div className="col-4">
+          <div className="clearfix d-flex justify-content start">
+            <div className="float-left">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Mostly Cloudy"
+              />
+            </div>
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>
+              Precipitation:<strong>2%</strong>
+            </li>
+            <li>
+              Humidity:<strong>83%</strong>
+            </li>
+            <li>
+              Wind:<strong>7mph</strong>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 position-relative">
+          <div className="position-absolute top-0 start-50 text-end">
+            <h2>Weather</h2>
+            <ul>
+              <li>Tuesday 05:00</li>
+              <li>Mostly Cloudy</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
