272bd14cbab20c8350ecc223dfa370073a58765a
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-04 11:42:03 +0100

    updated package.json and imported axios for daily weather forecast

--- src/WeatherForecastDaily.js
+++ src/WeatherForecastDaily.js
@@ -1,7 +1,16 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecastDaily.css";
+import axios from "axios";
 export default function WeatherForecastDaily() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "866a208a73eeff02182218e9441647a1";
+  let lon = 55.4;
+  let lat = 3.4;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
