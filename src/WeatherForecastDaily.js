47b58ff79005b02cf89b166671d357d15a94773c
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-05 17:12:36 +0100

    Moved forecast api call inside function getForecast()

--- src/WeatherForecastDaily.js
+++ src/WeatherForecastDaily.js
@@ -12,6 +12,13 @@ export default function WeatherForecastDaily(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+  function getForecast() {
+    let apiKey = "866a208a73eeff02182218e9441647a1";
+    let lon = props.coords.lon;
+    let lat = props.coords.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -31,11 +38,7 @@ export default function WeatherForecastDaily(props) {
       </div>
     );
   } else {
-    let apiKey = "866a208a73eeff02182218e9441647a1";
-    let lon = props.coords.lon;
-    let lat = props.coords.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    getForecast();
     return null;
   }
 }
